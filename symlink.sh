31c453ab29d5b4dc4ab72e974b2c64d097daa6af
danial <danial.malik@arbisoft.com>
2019-03-08 12:54:36 +0500

    Fri Mar  8 12:54:36 PKT 2019

--- symlink.sh
+++ symlink.sh
@@ -10,6 +10,7 @@ mkdir ~/.config/terminator/
 # create symlinks
 sudo ln -sf $dotfiles_dir/vimrc ~/.vimrc
 sudo ln -sf $dotfiles_dir/bashrc ~/.bashrc
+sudo ln -sf $dotfiles_dir/profile ~/.profile
 sudo ln -sf $dotfiles_dir/zshrc ~/.zshrc
 sudo ln -sf $dotfiles_dir/aliases ~/.bash_aliases
 sudo ln -sf $dotfiles_dir/aliases ~/.zsh_aliases
